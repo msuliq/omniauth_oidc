e9b0949dd7bfd6314f2b153bf65cfd0728fe9e98
Suleyman Musayev <slmusayev@gmail.com>
2024-06-16 15:47:58 +0500

    add dependabot

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 ## [Released]
 
+## [0.1.1] - 2024-06-16
+- Add dependabot
+
 ## [0.1.0] - 2024-06-13
 - Initial release
--- test/test_omniauth_oidc.rb
+++ test/test_omniauth_oidc.rb
@@ -8,6 +8,6 @@ class TestOmniauthOidc < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
   end
 end
